f1e3e50c799771a8c602af8d4a83c620a2d0b711
Patricia Adamsson <patricia.adamsson@gmail.com>
2015-10-06 13:17:49 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php
+    // Metadata
+    $metaTitle = '';
+    $metaDescription = '';
+
+    // Page specifict
+    $siteLang = 'sv';
+    $pageTitle = 'Oops, 404!';
+    $pageImg = '404error.png';
+?>
+
+<?php require_once('assets/head.php'); ?>
+    <?php require_once('assets/sv/header.php'); ?>
+        <main class="main container--full bg">
+            <div class="page-error">
+                <h1><?php echo $pageTitle; ?></h1>
+                <p>Något gick fel. Prova att börja om från <a href="/">start</a>!</p>
+                <a href="/" class="error-link"><img src="ui/images/page-img/<?php echo $pageImg; ?>" alt="" /></a>
+            </div>
+        </main>
+
+<?php require_once('assets/sv/footer.php'); ?>
