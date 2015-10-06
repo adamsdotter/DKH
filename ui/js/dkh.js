fd4c40f6ef4fe417d54637d3c29718c8009ded33
Patricia Adamsson <patricia.adamsson@gmail.com>
2015-10-06 12:15:24 +0200

    Add ui

---
+++ ui/js/dkh.js
@@ -0,0 +1,61 @@
+(function() {
+  'use strict';
+
+  // Shorthand
+  var $ = document,
+
+  DKH = {
+    init: function() {
+      this.menu.init( $.getElementById('toggle-nav'), $.getElementById('site-menu') );
+    },
+
+    // Mobile nav
+    menu: {
+      init: function(button, ul) {
+        this.button = button;
+        this.ul = ul;
+        this.state = 'closed';
+
+        this.addEvents();
+      },
+
+      addEvents: function() {
+        var self = this,
+          trigger = self.button;
+
+        trigger.addEventListener('click', function() {
+          self.toggle();
+        });
+      },
+
+      // When clicking the trigger
+      toggle: function() {
+        if (this.state === 'closed') {
+          this.slideDown();
+        } else {
+          this.slideUp();
+        }
+      },
+
+      slideDown: function() {
+        var ul = this.ul;
+
+        ul.className = 'open';
+        this.state = 'open';
+      },
+
+      slideUp: function() {
+        var ul = this.ul;
+
+        ul.className = 'closed';
+        this.state = 'closed';
+      }
+    }
+  };
+
+  $.addEventListener( 'DOMContentLoaded', function () {
+      DKH.init();
+  }, false );
+
+})();
+
