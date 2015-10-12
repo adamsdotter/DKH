22eb5193ddaa95d77e57dafec8f56f462330f9ad
Patricia Adamsson <patricia.adamsson@gmail.com>
2015-10-12 11:47:10 +0200

    Update work text

--- lediga-jobb.php
+++ lediga-jobb.php
@@ -17,27 +17,17 @@
             <h1><?php echo $pageTitle; ?></h1>
             <div class="page-content">
                 <p>Vill du jobba på Doktor Kom Hem? Just nu söker vi:</p>
-                <h2>
-                    Specialist i Allmänmedicin / Vik. Distriksläkare
-                </h2>
-                <p>Vi söker dig som vill jobba på en liten och välfungerande husläkarmottagning på Kungsholmen.</p>
+                <h2>Distriktssköterska</h2>
+                <p>Doktor Kom Hem är en trevlig husläkarmottagning på Kungsholmen. Vi är ett glatt och härligt gäng som trivs med att jobba ihop.</p>
+                <p>Hos oss hittar du Specialistläkare i allmänmedicin, distriktssköterska och mottagningssköterskor, erfaren receptionist och psykoterapeut med många års erfarenhet. Vår erfarna distriktssköterska ska nu pensionera sig och därför söker vi ny ersättare med diabeteskompetens.</p>
+                <h3>Om mottagningen</h3>
+                <p>Vi flyttade in i nybyggda lokaler 2011 vid Hornsbergsstrand. Detta är ett område som växer mycket snabbt och har en ung befolkning och många barn. Vi har ca 5000 listade patienter.</p>
+                <p>Vi bedriver sedvanlig läkar- och sköterskemottagning med stort intresse och kunskap för både astma/kol och diabetes. På mottagningen bedrivs även en hälsomottagning där vi erbjuder rådgivning kring hälsosamma levnadsvanor.</p>
+                <p>Patienterna tar vi hand om i ett gemensamt team. Vi har en liten hemsjukvård. Inom rehabgarantin jobbar vår psykoterapeut med KBT behandling.</p>
+                <h3>Kvalifikationer</h3>
+                <p>Du som söker är utbildad distriktssköterska. Goda språkkunskaper i svenska språket i såväl tal som skrift är ett krav. Du har servicetänkande och har patienten i fokus. Därtill trivs du med att arbeta självständigt samtidigt som du har fallenhet för smidigt samarbete med kollegor. Vi uppskattar egna initiativ och idéer. Viktigt är att du är självgående samt har ett glatt humör. Vi ser gärna att du har diabetskompetens.</p>
                 <p>
-                    Vi har ett ständigt växande patientunderlag med många barnfamiljer och unga patienter. 
-                    Vår hemsjukvård är liten, vi gör hembesök när det behövs.
-                </p>
-                <p>
-                    2011 flyttade vi in i helt nybyggda lokaler. Mottagningen ligger invid Hornsbergsstrand, 
-                    ett nytt och lättillgängligt område på Kungsholmen med många restauranger i omgivningen.
-                </p>
-                <p>
-                    Hos oss jobbar kompetent och professionell personal som strävar efter att ge den bästa vården 
-                    och ett trevligt bemötande till alla patienter. 
-                    På mottagningen är vi två sjuksköterskor och en distriktssköterska, en psykoterapeut och en receptionist. 
-                    Just nu har vi en ordinarie- och en vikarierande specialist i allmänmedicin.
-                </p>
-                <p>
-                    Vill du vara med och utveckla denna välfungerade mottagning är du varmt välkommen att höra av dig till  
-                    Pascal Andersson, <a href="mailto:pascal.andersson@doktorkomhem.nu">pascal.andersson@doktorkomhem.nu</a>
+                    Vill du vara med och utveckla denna välfungerade mottagning är du varmt välkommen att höra av dig på <a href="mailto:ewabritt.andersson@doktorkomhem.nu">ewabritt.andersson@doktorkomhem.nu</a>
                 </p>
             </div>
             <figure class="page-img">
