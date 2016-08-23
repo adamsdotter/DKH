f005eb8cbb6be78486f7ae19c0ebc29451971a34
Patricia Adamsson <patricia.adamsson@gmail.com>
2016-08-23 18:47:12 +0200

    Add english version of new about page

--- about.php
+++ about.php
@@ -8,38 +8,24 @@
     $siteLang = 'en';
     $pageName = 'about'; // To set active in nav
     $pageTitle = 'About Doktor Kom Hem';
-    $pageImg = 'korridor.jpg';
 ?>
 
 <?php require_once('assets/head.php'); ?>
     <?php require_once('assets/en/header.php'); ?>
         <main class="main container--full bg">
             <h1><?php echo $pageTitle; ?></h1>
-            <div class="page-content cushycms"><p>Here at Doktor Kom Hem, you will meet friendly doctors and nurses with many years of experience.</p>
+            <div class="page-content no-image cushycms">
+              <p>Here at Doktor Kom Hem, you will meet friendly doctors and nurses with many years of experience.</p>
+              <h2>Our staff</h2>
+            </div>
 
-<p>We who work at the clinic:</p>
+            <?php require_once('assets/en/staff.php'); ?>
 
-<ul class="staff color-box">
-	<li><strong>Dr. Ewabritt Andersson</strong>, manager, general practitioner</li>
-	<li><strong>Dr. Kadir Kakili</strong>, general practitioner</li>
-	<li><strong>Dr. Eva Maria Lundell</strong>, general practitioner</li>
-	<li><strong>Magdalena &Aring;senblom</strong>,&nbsp;licensed district- and diabetes nurse</li>
-	<li><strong>Kerstin Eklund</strong>, licensed district nurse</li>
-	<li><strong>Maria Rollstedt</strong>, licensed nurse</li>
-	<li><strong>Malin K&auml;llgren</strong>, licensed&nbsp;nurse</li>
-	<li><strong>Inger Hagstr&ouml;m</strong>, licensed psychotherapist, CBT</li>
-	<li><strong>Pascal Andersson</strong>, receptionist</li>
-</ul>
-
-<h2>Why this name on a family practice?</h2>
-
-<p>&quot;Doktor Kom Hem&quot; means &quot;Doctor Come Home&quot; in Swedish. We know how valuable a home visit can be, both for security and personal contact. Some people may have difficultyies getting to and from the clinic, elder people as well as busy families.</p>
-
-<p>Our ambition is to provide plenty of room for a home visits for our listed patients at Kungsholmen. But we are also, despite the name, a completely ordinary Family Practice.</p>
-</div>
-            <figure class="page-img">
-                <img src="<?php echo $images; ?>page-img/<?php echo $pageImg; ?>" alt="" />
-            </figure>
+            <div class="page-content no-image cushycms">
+              <h2>Why this name on a family practice?</h2>
+              <p>&quot;Doktor Kom Hem&quot; means &quot;Doctor Come Home&quot; in Swedish. We know how valuable a home visit can be, both for security and personal contact. Some people may have difficultyies getting to and from the clinic, elder people as well as busy families.</p>
+              <p>Our ambition is to provide plenty of room for a home visits for our listed patients at Kungsholmen. But we are also, despite the name, a completely ordinary Family Practice.</p>
+            </div>
         </main>
 
 <?php require_once('assets/en/footer.php'); ?>
---
+++ assets/en/staff.php
@@ -0,0 +1,76 @@
+<div class="cushycms staff-list">
+  <ul class="staff">
+    <li>
+      <figure class="staff-img">
+          <img src="<?php echo $images; ?>staff/ewabritt.png" alt="Dr. Ewabritt Andersson" />
+      </figure>
+      <figcaption class="staff-name">
+          <strong>Dr. Ewabritt Andersson</strong> Manager, <br> General Practitioner
+      </figcaption>
+    </li>
+    <li>
+      <figure class="staff-img">
+          <img src="<?php echo $images; ?>staff/kadir.png" alt="Dr. Kadir Kakili" />
+      </figure>
+      <figcaption class="staff-name">
+          <strong>Dr. Kadir Kakili</strong> General Practitioner
+      </figcaption>
+    </li>
+    <li>
+      <figure class="staff-img">
+          <img src="<?php echo $images; ?>staff/evamaria.png" alt="Dr. Eva Maria Lundell" />
+      </figure>
+      <figcaption class="staff-name">
+        <strong>Dr. Eva Maria Lundell</strong> General Practitioner
+      </figcaption>
+    </li>
+    <li>
+      <figure class="staff-img">
+          <img src="<?php echo $images; ?>staff/no-img.png" alt="Magdalena &Aring;senblom" />
+      </figure>
+      <figcaption class="staff-name">
+          <strong>Magdalena &Aring;senblom</strong> Licensed district- and diabetes nurse
+      </figcaption>
+    </li>
+    <li>
+      <figure class="staff-img">
+          <img src="<?php echo $images; ?>staff/no-img.png" alt="Kerstin Eklund" />
+      </figure>
+      <figcaption class="staff-name">
+          <strong>Kerstin Eklund</strong> Licensed district nurse
+      </figcaption>
+    </li>
+    <li>
+      <figure class="staff-img">
+          <img src="<?php echo $images; ?>staff/maria.png" alt="Maria Rollstedt" />
+      </figure>
+      <figcaption class="staff-name">
+          <strong>Maria Rollstedt</strong> Licensed nurse
+      </figcaption>
+    </li>
+    <li>
+      <figure class="staff-img">
+          <img src="<?php echo $images; ?>staff/malin.png" alt="Malin K&auml;llgren" />
+      </figure>
+      <figcaption class="staff-name">
+          <strong>Malin K&auml;llgren</strong> Licensed nurse
+      </figcaption>
+    </li>
+    <li>
+      <figure class="staff-img">
+          <img src="<?php echo $images; ?>staff/no-img.png" alt="Inger Hagstr&ouml;m" />
+      </figure>
+      <figcaption class="staff-name">
+          <strong>Inger Hagstr&ouml;m</strong> Licensed psychotherapist, CBT
+      </figcaption>
+    </li>
+    <li>
+      <figure class="staff-img">
+          <img src="<?php echo $images; ?>staff/pascal.png" alt="Pascal Andersson" />
+      </figure>
+      <figcaption class="staff-name">
+          <strong>Pascal Andersson</strong> Receptionist
+      </figcaption>
+    </li>
+  </ul>
+</div>
