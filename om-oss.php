77fcd089e81af8d4c108d4c8f15f93c78f777e4d
Patricia Adamsson <patricia.adamsson@gmail.com>
2016-08-23 18:41:32 +0200

    New about us page, SV

--- om-oss.php
+++ om-oss.php
@@ -8,40 +8,22 @@
     $siteLang = 'sv';
     $pageName = 'about'; // To set active in nav
     $pageTitle = 'Om Doktor Kom Hem';
-    $pageImg = 'korridor.jpg';
 ?>
 
 <?php require_once('assets/head.php'); ?>
     <?php require_once('assets/sv/header.php'); ?>
         <main class="main container--full bg">
             <h1><?php echo $pageTitle; ?></h1>
-            <div class="page-content cushycms"><p>Hos oss p&aring; Doktor Kom Hem m&ouml;ts du av husl&auml;kare och sk&ouml;terskor med m&aring;nga &aring;rs erfarenhet.</p>
-
-<p>Vi som jobbar p&aring; mottagningen &auml;r:</p>
-
-<ul class="staff color-box">
-	<li><strong>Dr. Ewabritt Andersson</strong>, verksamhetschef, specialist i allm&auml;nmedicin</li>
-	<li><strong>Dr. Kadir Kakili</strong>, specialist i allm&auml;nmedicin</li>
-	<li><strong>Dr. Eva Maria Lundell</strong>, specialist i allm&auml;nmedicin</li>
-	<li><strong>Magdalena &Aring;senblom</strong>, Leg. distrikts- och diabetessk&ouml;terska</li>
-	<li><strong>Kerstin Eklund</strong>, Leg. distriktssk&ouml;terska</li>
-	<li><strong>Maria Rollstedt</strong>, Leg. sjuksk&ouml;terska</li>
-	<li><strong>Malin K&auml;llgren</strong>, Leg. sjuksk&ouml;terska</li>
-	<li><strong>Inger Hagstr&ouml;m</strong>, Leg.&nbsp;psykoterapeut, KBT</li>
-	<li><strong>Pascal Andersson</strong>, receptionist</li>
-</ul>
-
-<p>Vi har avtal med Landstinget enligt <a href="/vardval-stockholm">V&aring;rdval Stockholm</a>, d&auml;rf&ouml;r kan du lista dig hos oss. Mer information finner du <a href="/vardval-stockholm">h&auml;r</a>.</p>
-
-<h2>Varf&ouml;r detta namn p&aring; en husl&auml;karmottagning?</h2>
-
-<p>Vi vet hur v&auml;rdefullt ett hembes&ouml;k kan vara, b&aring;de f&ouml;r tryggheten och den personliga kontakten. Somliga kan ha sv&aring;rt att ta sig till mottagningen, det g&auml;ller s&aring;v&auml;l &auml;ldre som stressade barnfamiljer.</p>
-
-<p>V&aring;r ambition &auml;r att ge god plats f&ouml;r just hembes&ouml;k &aring;t v&aring;ra listade patienter p&aring; Kungsholmen. Men vi &auml;r ocks&aring;, trots namnet, en helt vanlig husl&auml;karmottagning som flyttat in p&aring; Lindhagensgatan 140-142.</p>
-</div>
-            <figure class="page-img">
-                <img src="<?php echo $images; ?>page-img/<?php echo $pageImg; ?>" alt="" />
-            </figure>
-        </main>
-
-<?php require_once('assets/sv/footer.php'); ?>
+            <div class="page-content no-image cushycms">
+              <p>Hos oss p&aring; Doktor Kom Hem m&ouml;ts du av husl&auml;kare och sk&ouml;terskor med m&aring;nga &aring;rs erfarenhet.</p>
+              <p>Vi har avtal med Landstinget enligt <a href="/vardval-stockholm">V&aring;rdval Stockholm</a>, d&auml;rf&ouml;r kan du lista dig hos oss. Mer information finner du <a href="/vardval-stockholm">h&auml;r</a>.</p>
+              <h2>Vår personal</h2>
+            </div>
+            <?php require_once('assets/sv/staff.php'); ?>
+            <div class="page-content no-image cushycms">
+              <h2>Varf&ouml;r detta namn p&aring; en husl&auml;karmottagning?</h2>
+              <p>Vi vet hur v&auml;rdefullt ett hembes&ouml;k kan vara, b&aring;de f&ouml;r tryggheten och den personliga kontakten. Somliga kan ha sv&aring;rt att ta sig till mottagningen, det g&auml;ller s&aring;v&auml;l &auml;ldre som stressade barnfamiljer.</p>
+              <p>V&aring;r ambition &auml;r att ge god plats f&ouml;r just hembes&ouml;k &aring;t v&aring;ra listade patienter p&aring; Kungsholmen. Men vi &auml;r ocks&aring;, trots namnet, en helt vanlig husl&auml;karmottagning som flyttat in p&aring; Lindhagensgatan 140-142.</p>
+            </div>
+          </main>
+    <?php require_once('assets/sv/footer.php'); ?>
