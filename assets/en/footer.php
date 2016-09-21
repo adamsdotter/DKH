fef6b477e7722642d121e4e6a896af451e1fbb80
Patricia Adamsson <patricia.adamsson@gmail.com>
2016-09-21 12:37:01 +0200

    Remove FB

--- assets/en/footer.php
+++ assets/en/footer.php
@@ -4,11 +4,10 @@
                         <li class="site-footer-item">Phone: <a href="tel:+46-86569700">08-656 97 00</a></li>
                         <li class="site-footer-item">Fax: 08-656 97 05</li>
                         <li class="site-footer-item"><a title="Skicka e-post till oss!" href="mailto:kontakt@doktorkomhem.nu">kontakt@doktorkomhem.nu</a></li>
-                        <li class="site-footer-item">
+                        <li class="site-footer-item last-item">
                             <span>Lindhagensg. 140-142</span>,
-                            <span>112 15</span> 
-                            <span >Stockholm</span></li>
-                        <li class="site-footer-item last-item"><a class="icon-fb" target="_blank" href="https://www.facebook.com/doktorkomhem" title="Doktor Kom Hem at Facebook"><span>Facebook</span></a>
+                            <span>112 15</span>
+                            <span>Stockholm</span>
                         </li>
                     </ul>
                 </div>
