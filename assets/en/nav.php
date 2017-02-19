0f2aaaaf678554585053fe31b9165696ac54a9a8
Patricia Adamsson <patricia.adamsson@gmail.com>
2017-02-19 14:51:48 +0100

    Add sticky wrapper

--- assets/en/nav.php
+++ assets/en/nav.php
@@ -1,30 +1,32 @@
-    <div class="nav-toggler">
-        <button type="button" id="toggle-nav">Menu</button>
-    </div>
-    <nav class="site-nav">
-        <div class="container--full">
-            <ul id="site-menu" accesskey="2">
-                <li <?php if($pageName === 'start') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/start" title="Startpage">Start</a>
-                </li>
-                <li <?php if($pageName === 'about') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/about" title="About us">About us</a>
-                </li>
-                <li <?php if($pageName === 'book') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/appointments" title="Appointments">Appointments</a>
-                </li>
-                <li <?php if($pageName === 'prices') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/patient-fees" title="Patient fees">Patient fees</a>
-                </li>
-                <li <?php if($pageName === 'vardval') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/choose-doctor" title="Vårdval Stockholm - choose doctor">Vårdval Stockholm</a>
-                </li>
-                <li <?php if($pageName === 'contact') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/contact" title="Contact">Contact</a>
-                </li>
-                <li>
-                    <a class="icon-flag flag--swe site-nav-link" href="/" title="På svenska"><span >På svenska</span></a>
-                </li>
-            </ul>
+    <div class="nav-container">
+        <div class="nav-toggler">
+            <button type="button" id="toggle-nav">Menu</button>
         </div>
-    </nav>
+        <nav class="site-nav">
+            <div class="container--full">
+                <ul id="site-menu" accesskey="2">
+                    <li <?php if($pageName === 'start') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/start" title="Startpage">Start</a>
+                    </li>
+                    <li <?php if($pageName === 'about') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/about" title="About us">About us</a>
+                    </li>
+                    <li <?php if($pageName === 'book') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/appointments" title="Appointments">Appointments</a>
+                    </li>
+                    <li <?php if($pageName === 'prices') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/patient-fees" title="Patient fees">Patient fees</a>
+                    </li>
+                    <li <?php if($pageName === 'vardval') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/choose-doctor" title="Vårdval Stockholm - choose doctor">Vårdval Stockholm</a>
+                    </li>
+                    <li <?php if($pageName === 'contact') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/contact" title="Contact">Contact</a>
+                    </li>
+                    <li>
+                        <a class="icon-flag flag--swe site-nav-link" href="/" title="På svenska"><span >På svenska</span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
--- assets/sv/nav.php
+++ assets/sv/nav.php
@@ -1,33 +1,35 @@
-    <div class="nav-toggler">
-        <button type="button" id="toggle-nav">Meny</button>
-    </div>
-    <nav class="site-nav">
-        <div class="container--full">
-            <ul id="site-menu" accesskey="2">
-                <li <?php if($pageName === 'start') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/" title="Startsidan">Start</a>
-                </li>
-                <li <?php if($pageName === 'about') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/om-oss" title="Om oss">Om oss</a>
-                </li>
-                <li <?php if($pageName === 'book') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/tidsbokning" title="Tidsbokning">Tidsbokning</a>
-                </li>
-                <li <?php if($pageName === 'prices') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/patientavgifter" title="Patientavgifter">Patientavgifter</a>
-                </li>
-                <li <?php if($pageName === 'vardval') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/vardval-stockholm" title="Vårdval Stockholm">Vårdval Stockholm</a>
-                </li>
-                <li <?php if($pageName === 'contact') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/kontakt" title="Kontakta oss">Kontakt</a>
-                </li>
-                <li <?php if($pageName === 'jobs') echo 'class="active"'; ?>>
-                    <a class="site-nav-link" href="/lediga-jobb" title="Jobba hos oss">Lediga jobb</a>
-                </li>
-                <li>
-                    <a class="icon-flag flag--eng site-nav-link" href="/start" title="In English"><span>In English</span></a>
-                </li>
-            </ul>
+    <div class="nav-container">
+        <div class="nav-toggler">
+            <button type="button" id="toggle-nav">Meny</button>
         </div>
-    </nav>
+        <nav class="site-nav">
+            <div class="container--full">
+                <ul id="site-menu" accesskey="2">
+                    <li <?php if($pageName === 'start') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/" title="Startsidan">Start</a>
+                    </li>
+                    <li <?php if($pageName === 'about') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/om-oss" title="Om oss">Om oss</a>
+                    </li>
+                    <li <?php if($pageName === 'book') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/tidsbokning" title="Tidsbokning">Tidsbokning</a>
+                    </li>
+                    <li <?php if($pageName === 'prices') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/patientavgifter" title="Patientavgifter">Patientavgifter</a>
+                    </li>
+                    <li <?php if($pageName === 'vardval') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/vardval-stockholm" title="Vårdval Stockholm">Vårdval Stockholm</a>
+                    </li>
+                    <li <?php if($pageName === 'contact') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/kontakt" title="Kontakta oss">Kontakt</a>
+                    </li>
+                    <li <?php if($pageName === 'jobs') echo 'class="active"'; ?>>
+                        <a class="site-nav-link" href="/lediga-jobb" title="Jobba hos oss">Lediga jobb</a>
+                    </li>
+                    <li>
+                        <a class="icon-flag flag--eng site-nav-link" href="/start" title="In English"><span>In English</span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
